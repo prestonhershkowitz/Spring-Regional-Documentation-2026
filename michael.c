d72244b290935637ca409158230e2673983dc36c
prestonhershkowitz <preston.hershkowitz@gmail.com>
2026-04-15 15:50:34 -0500

    michael.c

---
+++ michael.c
@@ -0,0 +1,120 @@
+    //wait_for_light(1);
+    shut_down_in(120);
+    enable_servos();
+    int arm = 2;
+    int up = 1500; //Value for arm to be raise
+    int closed = 200;
+    int claw = 3;
+    int down = 500;
+    int open = 2000;
+    lblack(1);
+    /*
+    set_servo_position(claw,open);
+    set_servo_position(arm,1650);
+   
+    set_servo_position(claw,2100);
+    forward(50);
+   
+    r(89);
+    ss(arm,1700,10);
+    msleep(10);
+    lineup(0,1);
+    r(20);
+    forward(2000);
+    l(20);
+    lineup(0,1);
+    msleep(10);
+    r(0.5);
+    ss(arm,1500,10);
+    forward(5000);//going foward to cube
+    r(1);
+    forward(1200);
+    ss(claw,900,10);
+    forward(1500);
+    p(1);
+    backward(500);
+    ss(arm,800,10);
+    p(0.1);
+    ss(claw,open,10);
+    msleep(100);
+    ss(arm,1600,10);
+    lineup(0,1);
+    forward(500);
+    l(2);
+   
+   
+   
+   
+    //starting getting pom poms
+   
+    ss(arm,1650,10);
+    r(20);
+    backward(1300);//backing up to get to lineup with pom poms
+    lineup(0,1);
+   
+    forward(100);
+    l(90);
+    lineup(0,1);
+    forward(1080);
+    r(90);
+    lineup(0,1);
+   
+    forward(1200);
+    backward(1300);
+    ss(2,down,6);
+    forward(1100);
+    ss(claw,0,5);
+    ss(arm,1700,5);
+    l(8);
+   
+lineup(0,1);
+    forward(400);
+    r(90);
+    forward(1800);
+    ss(2,1550,5);
+    set_servo_position(claw,open);
+    msleep(500);
+   
+    //now getting second round of putting pom poms in the first basket
+    backward(1800);
+    l(89);
+    lineup(0,1);
+    ao();
+    ss(arm,down,6);
+    forward(3600);
+    ss(claw,0,5);
+    l(5);
+    lineup(0,1);
+    forward(400);
+    ss(arm,1650,10);
+    msleep(100);
+    r(90);
+    forward(2200);
+    set_servo_position(claw,open);
+    msleep(500);
+ 
+    //Now getting third set of poms
+    backward(1900);
+    l(87);
+    lineup(0,1);
+    ao();
+    ss(arm,down,6);
+    forward(4900);
+    r(2);
+    forward(1000);
+    ss(claw,0,5);
+    lineup(0,1);
+    forward(400);
+    ss(arm,1650,10);
+    msleep(100);
+    r(90);
+    forward(2200);
+    set_servo_position(claw,open);
+    msleep(500);
+ 
+    backward(1700);
+    l(87);
+lineup(0,1);
+   
+    forward(10000);
+    */
